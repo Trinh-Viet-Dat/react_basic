99bb044bff24f7be97741ba4c5dcbf3a0aa2851a
TrinhDat <cv.trinhvietdat1805@gmail.com>
2022-03-29 10:47:01 +0700

    ToDo

--- src/ToDo.js
+++ src/ToDo.js
@@ -1,25 +1,64 @@
+import React from "react";
 import "./styles.scss";
 
-function ToDo() {
-    return (
-        <div className="ToDo">
-            <h1> ToDos </h1>
-            <div className="Add">
-                <div id="header-add">Add a Task</div>
-                <div className="content-add">
-                    <hr />
-                    <p>Item</p>
-                    <input
-                        type="text"
-                        id="input-add"
-                        placeholder="Nhập Task...."
-                    />{" "}
-                    <br />
-                    <button type="button">Submit</button>
+class ToDo extends React.Component {
+    render() {
+        return (
+            <div className="ToDo">
+                <h1> ToDos </h1>
+                <div className="Add">
+                    <div className="header-add">Add a Task</div>
+                    <div className="content-add">
+                        <p>Item</p>
+                        <input
+                            type="text"
+                            className="input-add"
+                            placeholder="What do you want to do ?"
+                        />
+                        <br />
+                        <button className="button-add">Submit</button>
+                    </div>
+                </div>
+                <br />
+                <div className="Task">
+                    <div className="header-task">Task</div>
+                    <div className="add-task">
+                        <table>
+                            <thead>
+                                <tr>
+                                    <th>Item</th>
+                                    <th>Status</th>
+                                    <th>Action</th>
+                                </tr>
+                            </thead>
+                            <tbody>
+                                <tr>
+                                    <td>Buy Ben</td>
+                                    <td>Not Completed</td>
+                                    <td>
+                                        <button id="conpleted">
+                                            Completed
+                                        </button>
+                                        <button id="delete">Delete</button>
+                                    </td>
+                                </tr>
+                                <tr>
+                                    <td>Buy Ben</td>
+                                    <td>Not Completed</td>
+                                    <td>
+                                        <button id="conpleted">
+                                            Completed
+                                        </button>
+                                        <button id="delete">Delete</button>
+                                    </td>
+                                </tr>
+                            </tbody>
+                        </table>
+                    </div>
                 </div>
             </div>
-        </div>
-    );
+        );
+    }
 }
 
 export default ToDo;
