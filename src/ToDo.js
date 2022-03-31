b0f908b08a5285fdb76150fa3b711eec6b74ea6e
TrinhDat <cv.trinhvietdat1805@gmail.com>
2022-03-31 14:09:34 +0700

    ToDo

--- src/ToDo.js
+++ src/ToDo.js
@@ -1,93 +1,117 @@
 import React, { useState } from "react";
 import "./styles.scss";
 function ToDo() {
-    const initTodo = [
-        {
-            item: "",
-            status: "",
-        },
-    ];
-    const [state, setState] = useState(initTodo);
-
-    const [item, setItem] = useState({
-        inputItem: "",
-    });
-    const handleOnchangeInput = (event) => {
-        const { value, name } = event.target;
-        console.log(event.target.value);
-        setItem({
-            ...item,
-            [name]: value,
-        });
-    };
-    const handleSubmit = () => {
-        let newitem = item.inputItem;
-        let states = [...state];
-        if (!newitem) {
-            alert("Nhập vào todo !");
-            return;
-        } else {
-            let newArr = {
-                item: newitem,
-                status: "ascs",
-            };
-            states.push(newArr);
-            setState(states);
-        }
-    };
-    return (
-        <div className="Todo">
-            <h1> ToDos </h1>
-            <div className="add">
-                <div className="header-add">Add a Task</div>
-                <div className="content-add">
-                    <p>Item</p>
-                    <input
-                        type="text"
-                        className="input-add"
-                        name="inputItem"
-                        placeholder="What do you want to do ?"
-                        value={item.inputItem}
-                        onChange={handleOnchangeInput}
-                    />
-                    <br />
-                    <button className="button-add" onClick={handleSubmit}>
-                        Submit
-                    </button>
-                </div>
-            </div>
-            <br />
-            <div className="Task">
-                <div className="header-task">Task</div>
-                <div className="add-task">
-                    <table>
-                        <thead>
-                            <tr>
-                                <th>Item</th>
-                                <th>Status</th>
-                                <th>Action</th>
-                            </tr>
-                        </thead>
-                        <tbody>
-                            {state.map((item, index) => (
-                                <tr key={index}>
-                                    <td>{item.item}</td>
-                                    <td>{item.status}</td>
-                                    <td>
-                                        <button id="conpleted">
-                                            Completed
-                                        </button>
-                                        <button id="delete">Delete</button>
-                                        <button id="edit">Edit</button>
-                                    </td>
-                                </tr>
-                            ))}
-                        </tbody>
-                    </table>
-                </div>
-            </div>
-        </div>
-    );
+	const initTodo = [
+		{
+			item: "avc",
+			status: "aá",
+		},
+	];
+	const [todo, setTodo] = useState(initTodo);
+	const [input_item, set_input_item] = useState({
+		inputItem: "",
+	});
+	const handleInputItem = (e) => {
+		console.log(e.target.value);
+		const { value, name } = e.target;
+		set_input_item({
+			...input_item,
+			[name]: value,
+		});
+	};
+	const handleSubmit = () => {
+		let newitem = input_item.inputItem;
+		let newtodo = [...todo];
+		const arr = {
+			item: newitem,
+			status: "abc",
+		};
+		newtodo.push(arr);
+		setTodo(newtodo);
+		input_item.inputItem = " ";
+	};
+	const handleDelete = (index) => {
+		const newitem = [...todo];
+		newitem.splice(index, 1);
+		setTodo(newitem);
+	};
+	const handleEdit = (index) => {
+		let newinput = prompt("Nhập công việc mới : ", "your work here....");
+		let arr = {
+			item: newinput,
+			status: "oke",
+		};
+		let newarr = [...todo];
+		newarr.splice(index, 1, arr);
+		setTodo(newarr);
+	};
+	return (
+		<div className="Todo">
+			<h1> ToDos </h1>
+			<div className="add">
+				<div className="header-add">Add a Task</div>
+				<div className="content-add">
+					<p>Item</p>
+					<input
+						type="text"
+						className="input-add"
+						name="inputItem"
+						value={input_item.inputItem}
+						placeholder="What do you want to do ?"
+						onChange={handleInputItem}
+					/>
+					<br />
+					<button className="button-add" onClick={handleSubmit}>
+						Submit
+					</button>
+				</div>
+			</div>
+			<br />
+			<div className="Task">
+				<div className="header-task">Task</div>
+				<div className="add-task">
+					<table>
+						<thead>
+							<tr>
+								<th>Item</th>
+								<th>Status</th>
+								<th>Action</th>
+							</tr>
+						</thead>
+						<tbody>
+							{todo.map((item, index) => (
+								<tr key={index}>
+									<td>{item.item}</td>
+									<td>{item.status}</td>
+									<td>
+										<button id="conpleted">
+											Completed
+										</button>
+										<button
+											id="delete"
+											onClick={(index) =>
+												handleDelete(index)
+											}
+										>
+											Delete
+										</button>
+										<button
+											id="edit"
+											onClick={(index) =>
+												handleEdit(index)
+											}
+										>
+											Edit
+										</button>
+									</td>
+								</tr>
+							))}
+						</tbody>
+					</table>
+				</div>
+			</div>
+		</div>
+	);
 }
 
 export default ToDo;
